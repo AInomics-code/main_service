362f93f0d20a4b0fbaa34b6bded7c70f0b282098
Jeff <jeffreysaa@outlook.com>
2025-08-15 20:43:38 -0500

    feat: unnecesary variables

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     OPENAI_KEY: str
-    PG_URL: str
-    MYSQL_URL: str
     SQLSERVER_URL: str
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
